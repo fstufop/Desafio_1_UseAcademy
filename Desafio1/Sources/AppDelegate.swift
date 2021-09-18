f3541a38a3344795372dc1311bedcd6026af5b79
fstufop <fstufop@gmail.com>
2021-09-18 13:07:00 -0300

    feat: organização das classes

---
+++ Desafio1/Sources/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Desafio1
+//
+//  Created by Filipe Simoes Teodoro on 18/09/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
